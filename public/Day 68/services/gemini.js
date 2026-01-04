360af9aebfdae39dd37128d3021a8e4c82ce2eaf
Sri Madhuri <srimadhuri2018@gmail.com>
2026-01-04 19:45:58 +0530

    updated api key

--- public/Day 68/services/gemini.js
+++ public/Day 68/services/gemini.js
@@ -1,5 +1,9 @@
 const API_URL =
+<<<<<<< HEAD
   "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=Your_API_key_here";
+=======
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=Your Api key here";
+>>>>>>> 03bfa1b (updated api key)
 
 export async function askGemini(text, imageBase64 = null) {
   const parts = [];
