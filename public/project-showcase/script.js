db0dd6b5e9aea8e0fe4dd4aa5aca38c991d59538
techy-ops <krishnakeerthana401@gmail.com>
2026-01-19 17:32:27 +0530

    Add project showcase page with preview cards

---
+++ public/project-showcase/script.js
@@ -0,0 +1,36 @@
+const projects = [
+  {
+    day: 1,
+    title: "Portfolio Website",
+    description: "Basic personal portfolio using HTML and CSS",
+    link: "../../Day01"
+  },
+  {
+    day: 2,
+    title: "Landing Page",
+    description: "Responsive landing page design",
+    link: "../../Day02"
+  },
+  {
+    day: 3,
+    title: "Todo List",
+    description: "Simple todo list using JavaScript",
+    link: "../../Day03"
+  }
+  // More projects can be added easily
+];
+
+const projectGrid = document.getElementById("projectGrid");
+
+projects.forEach(project => {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <h3>Day ${project.day}: ${project.title}</h3>
+    <p>${project.description}</p>
+    <a href="${project.link}" target="_blank">View Project →</a>
+  `;
+
+  projectGrid.appendChild(card);
+});
